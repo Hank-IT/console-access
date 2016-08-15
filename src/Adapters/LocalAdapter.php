18cdf8ce44aa38e4ad042adb6c537907d86c296c
MrCrankHank <mail@alexander-hank.de>
2016-08-15 16:16:57 +0200

    Add docs & service provider

--- src/Adapters/LocalAdapter.php
+++ src/Adapters/LocalAdapter.php
@@ -1,15 +1,52 @@
 <?php
 
+/**
+ * This file contains the LocalAdapter class.
+ * It implements the access to the console of
+ * the local server.
+ *
+ * PHP version 5.6
+ *
+ * @category Console
+ * @package  MrCrankHank\ConsoleAccess\Adapters
+ * @author   Alexander Hank <mail@alexander-hank.de>
+ * @license  Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0.txt
+ * @link     null
+ */
+
 namespace MrCrankHank\ConsoleAccess\Adapters;
 
 use MrCrankHank\ConsoleAccess\Interfaces\AdapterInterface;
 use Closure;
 
+/**
+ * Class LocalAdapter
+ *
+ * PHP version 5.6
+ *
+ * @category Console
+ * @package  MrCrankHank\ConsoleAccess\Adapters
+ * @author   Alexander Hank <mail@alexander-hank.de>
+ * @license  Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0.txt
+ * @link     null
+ */
 class LocalAdapter implements AdapterInterface {
+    /**
+     * @var string
+     */
     private $exitStatus;
 
+    /**
+     * @var string
+     */
     private $output;
 
+    /**
+     * Run a command.
+     *
+     * @param string       $command Command which should be run
+     * @param Closure|null $live    Closure to capture the live output of the command
+     */
     public function run($command, Closure $live = null)
     {
         while (@ ob_end_flush());
@@ -26,16 +63,28 @@ class LocalAdapter implements AdapterInterface {
             }
         }
 
-        $this->output = fread($run, 4096);
+        $this->output = fread($run, 8192);
 
         $this->exitStatus = pclose($run);
     }
 
+    /**
+     * Return the output of the last command.
+     * Does not work well with long running commands.
+     * You should capture the live output instead.
+     *
+     * @return mixed
+     */
     public function getOutput()
     {
         return $this->output;
     }
 
+    /**
+     * Return the exist status of the last command
+     *
+     * @return mixed
+     */
     public function getExitStatus()
     {
         return $this->exitStatus;
--- src/ConsoleAccessServiceProvider.php
+++ src/ConsoleAccessServiceProvider.php
@@ -1,7 +1,61 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: alexander.hank
- * Date: 14.08.2016
- * Time: 16:37
+ * This file contains the ConsoleAccessServiceProvider class
+ * It makes the functionality available to laravel
+ *
+ * PHP version 5.6
+ *
+ * @category Console
+ * @package  MrCrankHank\ConsoleAccess
+ * @author   Alexander Hank <mail@alexander-hank.de>
+ * @license  Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0.txt
+ * @link     null
  */
+
+namespace MrCrankHank\ConsoleAccess;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ConsoleAccessServiceProvider
+ *
+ * @category Console
+ * @package  MrCrankHank\ConsoleAccess
+ * @author   Alexander Hank <mail@alexander-hank.de>
+ * @license  Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0.txt
+ * @link     null
+ */
+class ConsoleAccessServiceProvider extends ServiceProvider
+{
+    /**
+     * All commands in this array will be registered with laravel
+     *
+     * @var array
+     */
+    protected $commands = [];
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(ConsoleAccess::class, function($app, $parameters) {
+            return new ConsoleAccess($parameters['adapter']);
+        });
+
+        $this->commands($this->commands);
+    }
+}
