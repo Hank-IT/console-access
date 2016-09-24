80e2d7cc37920dc68e57d547f86e6449adfc702d
MrCrankHank <mail@alexander-hank.de>
2016-09-24 14:06:33 +0200

    Update interface

--- src/Interfaces/ConsoleAccessInterface.php
+++ src/Interfaces/ConsoleAccessInterface.php
@@ -8,19 +8,25 @@ interface ConsoleAccessInterface
 {
     public function __construct(AdapterInterface $adapter);
 
+    public function sudo($sudo = '/usr/bin/sudo');
+
+    public function bin($bin, $escape = true);
+
+    public function param($param, $hidden = false, $escape = true);
+
+    public function exec(Closure $live = null);
+
     public function getOutput();
 
     public function getExitStatus();
 
     public function getBin();
 
+    public function setPreExec(Closure $function);
+
+    public function setPostExec(Closure $function);
+
     public function getParams();
-
-    public function exec(Closure $live = null);
-
-    public function bin($bin);
-
-    public function param($param, $escape = true);
 
     public function getStart();
 
@@ -28,9 +34,5 @@ interface ConsoleAccessInterface
 
     public function getDuration();
 
-    public function sudo($sudo = '/usr/bin/sudo');
-
-    public function setPreExec(Closure $function);
-
-    public function setPostExec(Closure $function);
+    public function getCommand();
 }
